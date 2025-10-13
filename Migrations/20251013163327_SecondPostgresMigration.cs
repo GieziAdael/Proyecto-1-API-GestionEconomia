b31c2d7174ea1e00b90157c9590130093b79d63e
GieziTlax <163209576+GieziTlax@users.noreply.github.com>
2025-10-13 10:34:48 -0600

    Actualizacion 2

---
+++ Migrations/20251013163327_SecondPostgresMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_GestionEconomia.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondPostgresMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
